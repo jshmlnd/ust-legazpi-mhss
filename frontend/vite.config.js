0dcfa6b200a2c9f8fd2d7ee6b02eb9320dc03e2d
Teika <joshua.malonda11@gmail.com>
2026-06-25 23:29:18 +0800

    faah

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the limit to 1000 kB (1MB)
+    chunkSizeWarningLimit: 1000, 
+  },
 })

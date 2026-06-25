10f79eaf312f82d7a77447d7f68798834ce35416
Teika <joshua.malonda11@gmail.com>
2026-06-25 23:45:38 +0800

    ughhh

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,8 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
 export const useAuthStore = create((set) => ({
     authUser: null,
 

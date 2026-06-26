8ad363fbd127feca5859b6da2a98d3c4f1e952bd
joshua. <joshua.malonda11@gmail.com>
2026-06-26 13:16:19 +0800

    update axios (again) (2)

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({ baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/" , withCredentials: true })
+export const axiosInstance = axios.create({ baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api" , withCredentials: true })

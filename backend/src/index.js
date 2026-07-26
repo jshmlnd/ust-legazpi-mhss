987a36939ab45832e7ceeadf8875f7465219728b
jshmlnd <joshuaklein.malonda@ust-legazpi.edu.ph>
2026-07-26 13:39:05 +0800

    fix: update 8 files

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,6 @@ import analyticsRoutes from "./routes/analytics.route.js";
 import suggestionRoutes from "./routes/suggestion.route.js";
 import agoraRoutes from "./routes/agora.route.js";
 import callLogRoutes from "./routes/callLog.route.js";
-import adminRoutes from "./routes/admin.route.js";
 
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
@@ -46,7 +45,6 @@ app.use("/api/analytics", analyticsRoutes);
 app.use("/api/suggestions", suggestionRoutes);
 app.use("/api/agora", agoraRoutes);
 app.use("/api/call-logs", callLogRoutes);
-app.use("/api/admin", adminRoutes);
 
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
